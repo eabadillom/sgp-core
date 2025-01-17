782819d028bcc9a76a0864ea9145a1d7477e1b23
Esteban Badillo <ebadillom@gmail.com>
2025-01-17 12:57:30 -0600

    Correcciones a clases DAO

--- src/main/java/com/ferbo/sgp/dao/PaisDAO.java
+++ src/main/java/com/ferbo/sgp/dao/PaisDAO.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.ferbo.sgp.model.Pais;
-import com.ferbo.sgp.util.SGPException;
 
 public class PaisDAO extends BaseDAO<Pais, Integer> {
 
@@ -37,30 +36,6 @@ public class PaisDAO extends BaseDAO<Pais, Integer> {
         }
         
         return modelList;
-    }
-
-    public synchronized Pais obtenrPorId(Integer id) throws SGPException{
-        Pais resultado = null;
-        EntityManager em = null;
-        
-        try{ 
-            log.info("Comienza el proceso de obtener el registro por id");
-            em = super.getEntityManager();
-            em.getTransaction().begin();
-            resultado = em.find(Pais.class, id);
-            em.getTransaction().commit();
-            log.info("Finaliza el proceso de obtener el registro por id");
-        }
-        catch (Exception ex){
-            log.error("Hubo algun problema al obtener el registro por id. " + ex.getMessage());
-            super.rollback(em);
-            throw new SGPException("Hubo algun problema al obtener el registro");
-        }
-        finally{
-            super.close(em);
-        }
-        
-        return resultado;
     }
 
     public Pais buscarPorClave(String clavePais) {
